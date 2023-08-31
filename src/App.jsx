3fd535c4343517e729850ae9e6d274048ad8a8af
Souzaa <zinfegr@gmail.com>
2023-08-31 20:33:58 -0300

    Estilização de responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <h1 id='titulo'>Quiz Perguntas</h1>
       <Caixa />
-      <p>Criado por <a href="https://github.com/zinfex" style={{color: 'white'}}target='blank_'>@4r1thur</a></p>
+      <p>Desenvolvido por <a href="https://github.com/zinfex" style={{color: 'white'}}target='blank_'>@4r1thur</a></p>
     </>
   )
 }
